84fd564b473996afcda83f416750a39b7d75b42e
Vladislav_747 <zippo585@gmail.com>
2018-09-07 00:02:33 +0500

    Initial commit

---
+++ app/src/main/java/com/example/melo/fragments/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.melo.fragments;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    Button btnDialog_58;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
